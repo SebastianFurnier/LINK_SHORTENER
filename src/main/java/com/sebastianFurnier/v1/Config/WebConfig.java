4e85d8a74bb742be11508cb2945d2f044ffd3b99
SebastianFurnier <sfurnier@hotmail.com>
2024-12-10 09:41:52 -0300

    Fix bugs

--- src/main/java/com/sebastianFurnier/v1/Config/WebConfig.java
+++ src/main/java/com/sebastianFurnier/v1/Config/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("POST", "GET")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
